9e6a942ac17ac6b739373e0ef9222c10d77ba5a5
bbshark99 <blue.shark99@outlook.com>
2020-11-25 19:59:58 -0500

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="dropzone">
+      <Container 
+        group-name="group-1" 
+        @drop="onDropItem"
+      >
+        <div 
+          class="drop-item"
+          v-for="(item, itemIdx) in items"
+          v-bind:key="itemIdx"
+        >
+          <h5>{{ item }}</h5>
+        </div>
+
+        <h3>Drop Here</h3>
+      </Container>
+    </div>
+    <div class="dragzone">
+      <Container
+        group-name="group-1"
+        behaviour="copy"
+        :get-child-payload="getChildPayload"
+      >
+        <Draggable 
+          v-for="(library, libraryIdx) in libraries" 
+          v-bind:key="libraryIdx"
+        >
+          <div class="dragger">
+            <h5>{{ library }}</h5>
+          </div>
+        </Draggable>
+      </Container>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Container, Draggable } from 'vue-smooth-dnd'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Container,
+    Draggable
+  }, 
+  data() {
+    return {
+      libraries: [
+        'Drag me - 1',
+        'Drag me - 2',
+        'Drag me - 3',
+        'Drag me - 4',
+        'Drag me - 5',
+      ],
+      items: []
+    }
+  },
+  methods: {
+    onDropItem(dropResult) {
+      const { addedIndex: index } = dropResult;
+      this.items.splice(index, 0, dropResult.payload);
+    },
+    getChildPayload(index) {
+      return this.libraries[index];
+    }
   }
 }
 </script>
@@ -24,5 +76,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  display: grid;
+  grid-template-columns: 65% auto;
+}
+
+.dropzone {
+  margin: .25rem;
+  border: 1px dashed grey;
+
+  min-height: 100px;
+}
+
+.dropzone .smooth-dnd-container {
+  height: 100%;
+}
+
+.dragzone .dragger, .dropzone .drop-item {
+  border: 1px solid grey;
+  padding: .25rem;
+  margin: .25rem;
 }
 </style>
